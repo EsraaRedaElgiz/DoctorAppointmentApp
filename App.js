1a940d0d05e1375e8c45b7397bed0829202520b7
Youssef Mohamed <yo.mohamed2001@gmail.com>
2023-03-17 20:32:49 +0200

    add

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { COLORS } from './src/constants/Constants';
 import AppContainer from './src/navigation/AppContainer';
+import EditDoctorDetails from './Doctor/src/screens/EditDoctorDetails/EditDoctorDetails';
+import Compeleteinformation from './Doctor/src/screens/Compeleteinformation/Compeleteinformation'
 import {useSelector} from 'react-redux';
+import MedicalSheet from './src/screens/MedicalSheet/MedicalSheet';
 function App() {
-  const globalState = useSelector(state => state);
-  return <AppContainer isAuth={globalState.AuthReducer.isLoggedIn} />;
+  // const globalState = useSelector(state => state);
+  // return <AppContainer isAuth={globalState.AuthReducer.isLoggedIn} />;
+  return<Compeleteinformation/>
 }
 
 export default App;
--- Doctor/src/screens/EditDoctorDetails/EditDoctorDetails.js
+++ Doctor/src/screens/EditDoctorDetails/EditDoctorDetails.js
@@ -1,20 +1,32 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity ,ScrollView} from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
-import { PADDINGS, COLORS, FONTS } from '../../constants/Constants'
-import { HeaderNavigation } from '../../components/headerNavigation/HeaderNavigation'
-import GeneralPage from '../../components/GeneralPage/GeneralPage'
-import ProfileImage from '../../components/ProfileImage/ProfileImage'
-import DropDown from '../../components/DropDown/DropDown'
-import Reusabletextinput from '../../components/AppTextinput/AppTextinput'
+import { COLORS, FONTS, PADDINGS, RADIUS } from '../../../../src/constants/Constants';
+import { HeaderNavigation } from '../../../../src/components/headerNavigation/HeaderNavigation';
+import GeneralButton from '../../../../src/components/GeneralButton/GeneralButton';
+import GeneralPage from '../../../../src/components/GeneralPage/GeneralPage';
+import ProfileImage from '../../../../src/components/ProfileImage/ProfileImage';
+import DropDown from '../../../../src/components/DropDown/DropDown';
+import Reusabletextinput from '../../../../src/components/AppTextinput/AppTextinput';
 import { RFValue } from 'react-native-responsive-fontsize'
-import GeneralButton from '../../components/GeneralButton/GeneralButton'
 import RBSheet from 'react-native-raw-bottom-sheet'
 import * as ImagePicker from 'react-native-image-picker';
-import { requestCameraPermission } from '../../utils/CameraPermissin';
+import { requestCameraPermission } from '../../../../src/utils/CameraPermissin';
 import { Controller, useForm } from 'react-hook-form'
-import ScrollView from 'react-native/Libraries/Components/ScrollView/ScrollView'
 export default function EditDoctorDetails() {
- const { control, handleSubmit, formState: { errors } } = useForm({});
+ const { control, handleSubmit, formState: { errors } } = useForm({
+  defaultValues:{
+    spealization:'',
+    exp:'',
+    Location:'',
+    Adressdescription:'',
+    About:'',
+    Workdays:'',
+    price:'',
+    start:'',
+    end:'',
+    section:''
+  }
+});
  const onSubmit = data => console.log(data);
  const [photo_uri, setphoto_uri] = useState("");
  const Specialization = ["اسنان", "باطنة", "صدر", "عيون"]
@@ -84,20 +96,20 @@ export default function EditDoctorDetails() {
        control={control}
        name="spealization"
        rules={{
-        required: "التخصص يكون مطلوب"
+        required: true
        }}
-       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+       render={({ field: { value, onChange, onBlur },  }) => (
         <>
          <DropDown
           style={styles.dropDownMarginBottom}
           placeholder="التخصص"
           data={Specialization}
-          borderColor={error ? "red" : COLORS.gray}
+          borderColor={errors.spealization ? "#f00" : COLORS.gray}
           onSelect={onChange}
          />
-         {error && (
-          <Text style={{ color: "red", alignSelf: "flex-start" }}>{error.message || "error"}</Text>
-         )}
+          <Text style={{ color: "red", alignSelf: "flex-start" }}>
+             {errors.spealization?.type === 'required' ? "التخصص يكون مطلوب" : ''}
+          </Text>
         </>
        )}
       />
@@ -107,27 +119,27 @@ export default function EditDoctorDetails() {
        control={control}
        name="exp"
        rules={{
-        required: "الخبرة تكون مطلوبة",
+        required: true,
         validate: val => {
          if (val * 0 != 0) {
           return 'must number';
          }
         },
        }}
-       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+       render={({ field: { value, onChange, onBlur },  }) => (
         <>
          <Reusabletextinput
           value={value}
           placeholder="الخبرة"
           onChangeText={onChange}
           onBlur={onBlur}
-          bordercolor={error ? "red" : "gray"}
+          bordercolor={errors.exp ? "#f00" : COLORS.gray}
          />
          <Text style={{ color: "red", alignSelf: "flex-start" }}>
           {
-           error?.type === 'required'
+           errors.exp?.type === 'required'
             ? 'يجب ادخال الخبرة'
-            : error?.type === 'validate'
+            : errors.exp?.type === 'validate'
              ? 'يجب ادخال رقم'
              : ''}</Text>
         </>
@@ -140,66 +152,68 @@ export default function EditDoctorDetails() {
       control={control}
       name="Location"
       rules={{
-       required: "يجب توافر الموقع"
+       required: true
       }}
-      render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+      render={({ field: { value, onChange, onBlur },  }) => (
        <>
         <Reusabletextinput
          placeholder="الموقع"
          value={value}
          onChangeText={onChange}
          onBlur={onBlur}
-         bordercolor={error ? "red" : "gray"}
+         bordercolor={errors.Location ? "#f00" : COLORS.gray}
         />
-        {error && (
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error.message || "error"}</Text>
-        )}
+         <Text style={{ color: "red", alignSelf: "flex-start" }}>
+         {errors.Location?.type === 'required' ? "يجب توافر الموقع" : ""}
+          </Text>
        </>
       )}
      />
     </View>
     <View style={styles.bottominputview}>
-     <Controller
-      control={control}
-      name="Adress description"
-      rules={{
-       required: "يجب وصف العنوان"
-      }}
-      render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
-       <>
-        <Reusabletextinput
-         placeholder="وصف عنوان العيادة"
-         value={value}
-         onChangeText={onChange}
-         onBlur={onBlur}
-         bordercolor={error ? "red" : "gray"}
-        />
-        {error && (
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error.message || "error"}</Text>
-        )}
-       </>
-      )}
-     />
+    <Controller
+              control={control}
+              name="Adressdescription"
+              rules={{
+                required: true
+              }}
+              render={({ field: { value, onChange, onBlur },  }) => (
+                <>
+                  <Reusabletextinput
+                    placeholder="وصف عنوان العيادة"
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    bordercolor={errors.Adressdescription ? "#f00" : COLORS.gray}
+                  />
+                  <Text style={{ color: "red", alignSelf: "flex-start" }}>
+                    {errors.Adressdescription?.type === 'required' ? "يجب وصف العنوان" : ""}
+                  </Text>
+                </>
+              )}
+            />
     </View>
     <View style={styles.bottominputview}>
      <Controller
       control={control}
       name="About"
       rules={{
-       required: "السيرة الذاتية مطلوبة"
+       required: true
       }}
-      render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+      render={({ field: { value, onChange, onBlur },  }) => (
        <>
         <Reusabletextinput
          placeholder="السيرة الذاتية"
          value={value}
          onChangeText={onChange}
          onBlur={onBlur}
-         bordercolor={error ? "red" : "gray"}
+         bordercolor={errors.About ? "#f00" : COLORS.gray}
         />
-        {error && (
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error.message || "error"}</Text>
-        )}
+        
+         <Text style={{ color: "red", alignSelf: "flex-start" }}>
+         {errors.About?.type === 'required' ? "السيرة الذاتية مطلوبة" : ""}
+          </Text>
+        
        </>
       )}
      /></View>
@@ -208,7 +222,7 @@ export default function EditDoctorDetails() {
       control={control}
       name="Workdays"
       rules={{
-       required: "يجب وضع ايام العمل"
+       required: true
       }}
       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
        <>
@@ -217,11 +231,13 @@ export default function EditDoctorDetails() {
          value={value}
          onChangeText={onChange}
          onBlur={onBlur}
-         bordercolor={error ? "red" : "gray"}
+         bordercolor={errors.Workdays ? "#f00" : COLORS.gray}
         />
-        {error && (
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error.message || "error"}</Text>
-        )}
+        
+         <Text style={{ color: "red", alignSelf: "flex-start" }}>
+         {errors.Workdays?.type === 'required' ? "يجب وضع ايام العمل" : ''}
+          </Text>
+        
        </>
       )}
      /></View>
@@ -230,26 +246,26 @@ export default function EditDoctorDetails() {
       control={control}
       name="price"
       rules={{
-       required: "يجب وضع  سعر الكشف",
+       required:true,
        validate: val => {
         if (val * 0 != 0) {
          return 'must number';
         }
        },
       }}
-      render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+      render={({ field: { value, onChange, onBlur },  }) => (
        <>
         <Reusabletextinput
          placeholder="سعر الكشف"
          value={value}
          onChangeText={onChange}
          onBlur={onBlur}
-         bordercolor={error ? "red" : "gray"}
+         bordercolor={errors.price ? "#f00" : COLORS.gray}
         />
         <Text style={{ color: "red", alignSelf: "flex-start" }}>
-         {error?.type === 'required'
+         {errors.price?.type === 'required'
           ? 'يجب ادخال سعر الكشف'
-          : error?.type === 'validate'
+          : errors.price?.type === 'validate'
            ? 'يجب ادخال رقم'
            : ''}</Text>
        </>
@@ -262,25 +278,26 @@ export default function EditDoctorDetails() {
        control={control}
        name="start"
        rules={{
-        required: "يجب وضع البداية",
+        required: true,
         validate: val => {
          if (val * 0 != 0) {
           return 'must number';
          }
         },
        }}
-       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+       render={({ field: { value, onChange, onBlur }, }) => (
         <>
          <Reusabletextinput
           placeholder="البداية"
           value={value}
           onChangeText={onChange}
           onBlur={onBlur}
-          bordercolor={error ? "red" : "gray"}
+          bordercolor={errors.start ? "#f00" : COLORS.gray}
          />
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error?.type === 'required'
+         <Text style={{ color: "red", alignSelf: "flex-start" }}>
+          {errors.start?.type === 'required'
           ? 'يجب ادخال البداية'
-          : error?.type === 'validate'
+          : errors.start?.type === 'validate'
            ? 'يجب ادخال رقم'
            : ''}</Text>
         </>
@@ -292,25 +309,26 @@ export default function EditDoctorDetails() {
        control={control}
        name="end"
        rules={{
-        required: "يجب وضع النهاية",
+        required:true,
         validate: val => {
          if (val * 0 != 0) {
           return 'must number';
          }
         },
        }}
-       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+       render={({ field: { value, onChange, onBlur }, }) => (
         <>
          <Reusabletextinput
           placeholder="النهاية"
           value={value}
           onChangeText={onChange}
           onBlur={onBlur}
-          bordercolor={error ? "red" : "gray"}
+          bordercolor={errors.end ? "#f00" : COLORS.gray}
          />
-         <Text style={{ color: "red", alignSelf: "flex-start" }}>{error?.type === 'required'
+         <Text style={{ color: "red", alignSelf: "flex-start" }}>
+          {errors.end?.type === 'required'
           ? 'يجب ادخال النهاية'
-          : error?.type === 'validate'
+          : errors.end?.type === 'validate'
            ? 'يجب ادخال رقم'
            : ''}</Text>
         </>
@@ -329,18 +347,19 @@ export default function EditDoctorDetails() {
          }
         },
        }}
-       render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+       render={({ field: { value, onChange, onBlur },}) => (
         <>
          <Reusabletextinput
           placeholder="المدة"
           value={value}
           onChangeText={onChange}
           onBlur={onBlur}
-          bordercolor={error ? "red" : "gray"}
+          bordercolor={errors.section ? "#f00" : COLORS.gray}
          />
-         <Text style={{ color: "red", alignSelf: "flex-start", }}>{error?.type === 'required'
+         <Text style={{ color: "red", alignSelf: "flex-start", }}>
+          {errors.section?.type === 'required'
           ? 'يجب ادخال المدة'
-          : error?.type === 'validate'
+          : errors.section?.type === 'validate'
            ? 'يجب ادخال رقم'
            : ''}</Text>
         </>
@@ -348,10 +367,9 @@ export default function EditDoctorDetails() {
       />
      </View>
     </View>
-    <GeneralButton
-     title="تاكيد"
-     onPress={handleSubmit(onSubmit)}
-    />
+    <View style={styles.buttonViewStyle}>
+        <GeneralButton title="تأكيد" onPress={handleSubmit(onSubmit)} />
+      </View>
     <RBSheet
      ref={refRBSheet}
      height={RFValue(200)}
@@ -434,7 +452,7 @@ const styles = StyleSheet.create({
   justifyContent: "space-around",
  },
  bottominputview: {
-  marginBottom: RFValue(8)
+  marginBottom: RFValue(0)
  }, timeandsection: {
   width: "100%",
   alignItems: "center",
@@ -463,5 +481,9 @@ const styles = StyleSheet.create({
   height: RFValue(1),
   backgroundColor: COLORS.gray,
   width: '90%',
+ },
+ buttonViewStyle: {
+   backgroundColor: COLORS.white,
+   paddingBottom: PADDINGS.mdPadding
  },
 })
