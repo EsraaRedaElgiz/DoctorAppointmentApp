6bcfd19643bcad6a875627a0bb39e56880b74260
EsraaRedaElgiz <110185098+EsraaRedaElgiz@users.noreply.github.com>
2023-03-18 22:41:52 +0200

    message

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import AppContainer from './src/navigation/AppContainer';
 import AppointmentDetails from './Doctor/src/screens/AppointmentDetails/AppointmentDetails';
 import AddAppointmentBySecretary from './Doctor/src/screens/AddAppointmentBySecretary/AddAppointmentBySecretary';
 import {useSelector} from 'react-redux';
+import Compeleteinformation from './Doctor/src/screens/Compeleteinformation/Compeleteinformation';
 function App() {
    {/*const globalState = useSelector(state => state);
 return <AppContainer isAuth={globalState.AuthReducer.isLoggedIn} />;*/}
@@ -12,10 +13,10 @@ return (
       {/*<HomeDoctor/> */}
       {/* <DoctorViewProfile/> */}
       {/* <BookAppointment/> */}
-      {/*<Compeleteinformation/>*/}
+      <Compeleteinformation/>
       {/*<DoctorFilterAppointment/>*/}
       {/*AddAppointmentBySecretary/>*/}
-      <AppointmentDetails/>
+      {/*<AppointmentDetails/>*/}
       
     </>
   );
