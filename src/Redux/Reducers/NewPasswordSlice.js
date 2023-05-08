f77d61c8caf2ba42eb465349c184bda65bc2f813
aymangaballah <aymangaballah71@gmail.com>
2023-05-09 00:22:42 +0200

    Alert

--- src/Redux/Reducers/NewPasswordSlice.js
+++ src/Redux/Reducers/NewPasswordSlice.js
@@ -1,6 +1,6 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 import Axios from '../../utils/axios';
-
+import {Alert} from 'react-native';
 const initState = {
   isLoading: false,
   error: null,
@@ -10,7 +10,7 @@ export const changePassword = createAsyncThunk(
   async (args, thunkAPI) => {
     const {rejectWithValue, dispatch} = thunkAPI;
     try {
-      let response=""
+      let response = '';
       await Axios({
         method: 'POST',
         url: '/general/change_password.php',
@@ -19,18 +19,20 @@ export const changePassword = createAsyncThunk(
         .then(res => {
           if (res.status == 200) {
             if (res.data.success) {
-             response=res.data.success;
+              response = res.data.success;
             } else {
               console.log(res.data);
+              Alert.alert("خطأ فى كلمه المرور القديمه \n حاول مره اخرى");
             }
           } else {
-            alert('حدث خطأ اثناء الاتصال بالخادم من فضلك حاول مجددا');
+            Alert.alert('حدث خطأ اثناء الاتصال بالخادم من فضلك حاول مجددا');
           }
         })
         .catch(err => {
           console.log(err);
+          Alert.alert(' خطأ اثناء الاتصال بالخادم من فضلك حاول مجددا');// right
         });
-        return response;
+      return response;
     } catch (error) {
       console.log(rejectWithValue(error.message));
       return rejectWithValue(error.message);
@@ -44,16 +46,18 @@ const newPasswordSlice = createSlice({
     builder.addCase(changePassword.pending, (state, action) => {
       state.isLoading = true;
       state.error = null;
+      console.log('pending');
     }),
       builder.addCase(changePassword.fulfilled, (state, action) => {
         state.isLoading = false;
+        console.log('sucess');
       }),
       builder.addCase(changePassword.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
-      })
+        console.log('failed');
+      });
   },
 });
 export default newPasswordSlice.reducer;
-export const { } =
-  newPasswordSlice.actions;
+export const {} = newPasswordSlice.actions;
